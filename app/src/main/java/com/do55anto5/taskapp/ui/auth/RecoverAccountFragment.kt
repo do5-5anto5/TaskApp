c321f566cbe7461406fcb8f40097381fe083c8ac
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-04-17 15:30:16 -0300

    Recover screen

---
+++ app/src/main/java/com/do55anto5/taskapp/ui/auth/RecoverAccountFragment.kt
@@ -0,0 +1,28 @@
+package com.do55anto5.taskapp.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.do55anto5.taskapp.R
+import com.do55anto5.taskapp.databinding.FragmentRecoverAccountBinding
+
+class RecoverAccountFragment : Fragment() {
+
+    private var _bind: FragmentRecoverAccountBinding? = null
+    private val bind get() = _bind!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _bind = FragmentRecoverAccountBinding.inflate(inflater, container, false)
+        return bind.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _bind = null
+    }
+}
