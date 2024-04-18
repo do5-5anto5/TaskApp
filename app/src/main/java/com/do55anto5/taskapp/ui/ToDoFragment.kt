3a8a1bef95cdc0d510e5cc9203fcabd7e46e6e0c
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-04-18 20:54:12 -0300

    create Form Task layout

--- app/src/main/java/com/do55anto5/taskapp/ui/ToDoFragment.kt
+++ app/src/main/java/com/do55anto5/taskapp/ui/ToDoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.do55anto5.taskapp.R
 import com.do55anto5.taskapp.databinding.FragmentToDoBinding
 
@@ -20,6 +21,17 @@ class ToDoFragment : Fragment() {
     ): View {
         _bind = FragmentToDoBinding.inflate(inflater, container, false)
         return bind.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener(){
+        bind.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
     }
 
     override fun onDestroy() {
